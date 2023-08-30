e8147475888ef31234e4dec55ea3732466763c9c
Mehwish <mehwishgul715@gmail.com>
2023-08-30 10:12:45 +0500

    commit

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -43,7 +43,44 @@ $('.owl-carousel').owlCarousel({
   }
 })
 
+// ============================================number of words in paragraph
+// $(document).ready(function(){
+//   //length in characters
+// var maxLength = 150;
+// var ellipsestext = "...";
+// var moretext = "Read more";
+// var lesstext = "Read less";
+// $(".showReadMore").each(function(){
+//   //get the text of paragraph or div
+//   var myStr = $(this).text();
+  
+//  // check if it exceeds the maxLength limit
+//   if($.trim(myStr).length > maxLength){
+//       //get only limited string firts to show text on page load
+//       var newStr = myStr.substring(0, maxLength);
 
+//       //get remaining string         
+// var removedStr = myStr.substr(maxLength, $.trim(myStr).length - maxLength);
+//       // now append the newStr + "..."+ hidden remaining string
+//       var Newhtml = newStr + '<span class="moreellipses">' + ellipsestext+ '</span><span class="morecontent"><span>' + removedStr + '</span>&nbsp;&nbsp;<a href="javascript:void(0)" class="ReadMore">' + moretext + '</a></span>';
 
-
-
+//       $(this).html(Newhtml);
+      
+//   }
+// });
+// $(".ReadMore").click(function(){
+ 
+//   if($(this).hasClass("less")) {
+//       $(this).removeClass("less");
+//       $(this).html(moretext);
+//   }
+//    else {
+//       $(this).addClass("less");
+//       $(this).html(lesstext);
+//   }
+  
+//   $(this).parent().prev().toggle();
+//   $(this).prev().toggle();
+//   return false;
+// });
+// })
