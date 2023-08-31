e9dfd629c9b3935faa701567975d2a30f77e8366
Mehwish <mehwishgul715@gmail.com>
2023-08-31 08:53:55 +0500

    commit

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -86,3 +86,64 @@ $('.owl-carousel').owlCarousel({
 //   return false;
 // });
 // })
+
+// ==================================dropzone
+class DropZone {
+  constructor() {
+    this.dropZone = $('.drop-zone');
+
+    // Add/Remove .is-dragover when hover/leave
+    this.dropZone.on('dragover dragenter', () => this.dropZone.addClass('is-dragover'));
+    this.dropZone.on('dragleave dragend drop', () => this.dropZone.removeClass('is-dragover'));
+
+    this.dropZone.on('change', this.onchange.bind(this));
+
+    // TODO: .has-image has to be removed when remove all images
+    // TODO: Generate uniq id and add to input.has-image and .preview
+    //       in order to remove them both when click on .remove
+  }
+
+  // Hide input.receiver and insert the new one
+  onchange(e) {
+    this.dropZone.addClass('has-images');
+
+    // Rename input.receiver => input.has-image
+    const $receiver = $(e.target);
+    $receiver.removeClass('receiver').addClass('has-image');
+
+    // Add new .receiver
+    $('<input type="file" class="receiver">').prependTo(this.dropZone);
+
+    // Preview
+    const files = $receiver[0].files;
+    this.displayPreview(files);
+  }
+
+  displayPreview(files) {
+    for (const file of files) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const url = e.currentTarget.result;
+        this.template(url).appendTo(this.dropZone);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  template(url) {
+    return $(`
+      <div class="preview">
+        <div class="image">
+          <img src="${url}">
+        </div>
+        <div class="details">
+          <div class="remove">
+            <span class="bi bi-trash3-fill text-white"></span>
+          </div>
+        </div>
+      </div>
+    `);
+  }
+}
+
+new DropZone();
